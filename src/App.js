5267d7235fafc6590b7e974327d97efae74a41eb
Natalia605207 <natalyaobukhova87@gmail.com>
2023-11-24 14:44:50 +0300

    second commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
   },[])
 
   const getAdvice = async () => {
-    const response = await fetch(`http://www.boredapi.com/api/activity/`);
+    const response = await fetch(`https://www.boredapi.com/api/activity/`);
     const data = await response.json();
     setActivity(data.activity);
     setActivityType(data.type);
