041d1c6bf1ff6c0a35f21c6b8fb056d87853146c
Natalia605207 <natalyaobukhova87@gmail.com>
2024-09-06 23:50:51 -0400

    changed the fetch link

--- src/components/BottomPart.js
+++ src/components/BottomPart.js
@@ -4,7 +4,7 @@ function BottomPart() {
     return <div>
         <img className="happyPeople" src={happy} alt="happy" />
         <hr />
-        <p className="footer">Designed and developed by <a className="footer" href="https://natalia-musikhina-portfolio.glitch.me/" target="_blank" rel="noreferrer">Natalia Musikhina</a></p>
+        <p className="footer">Designed and developed by <a className="footer" href="https://natalia-webdev.com/" target="_blank" rel="noreferrer">Natalia Musikhina</a></p>
         <p className="footer">The data: <a className="footer" href="http://www.boredapi.com/api/activity/" target="_blank" rel="noreferrer">www.boredapi.com</a></p>
         <p className="footer">The images: <a className="footer" href="https://www.freepik.com/" target="_blank" rel="noreferrer">www.freepik.com</a></p>
     </div>
